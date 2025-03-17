6de8da870cc7d827ea5ff19e6369cf67017eb748
kyoredes <kyoredes@gmail.com>
2025-03-18 00:11:31 +1000

    users routers add

--- payapi/main.py
+++ payapi/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from payapi.authentication.routers import auth_router
+from payapi.users.routers import users_router
 
 app = FastAPI()
 
 # login, logout
 app.include_router(auth_router)
+app.include_router(users_router)
